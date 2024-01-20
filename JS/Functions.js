e69c3810f759e8a05c641ebb45374bf656990b7d
KV SUBBAIAH <110246839+subbuk8977@users.noreply.github.com>
2024-01-20 11:13:55 +0530

    Add files via upload

---
+++ JS/Functions.js
@@ -0,0 +1,9 @@
+function add(a,b)
+{
+	return a+b;
+}
+
+let a=10;
+let b=20;
+l
+document.write(add(a,b));
---
+++ JS/funct.js
@@ -0,0 +1,9 @@
+function square(n)
+{
+	ans=n*n;
+	return ans;
+}
+
+let number=102;
+result=square(number);
+console.log(result);
