6bd96de22ca102125a8e9905dbc5d6d6b274eec2
KV SUBBAIAH <110246839+subbuk8977@users.noreply.github.com>
2024-01-20 10:59:25 +0530

    Add files via upload

---
+++ JS/DOM.js
@@ -0,0 +1,10 @@
+
+var y=document.getElementsByClassName("cls1");
+console.log(y);
+
+var x=document.getElementById("One");
+console.log(x);
+
+
+var z=document.getElementsByTagName("h1");
+console.log(z);
