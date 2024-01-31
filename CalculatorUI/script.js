af5869bcf07111c46285f7739cde9ba9e4eaf57e
KV SUBBAIAH <110246839+subbuk8977@users.noreply.github.com>
2024-01-31 10:32:06 +0530

    Update script.js
    Added score variable externally .

--- CalculatorUI/script.js
+++ CalculatorUI/script.js
@@ -1,23 +1,22 @@
 
+let score=0;
 function isScore(){
 
     let val=Number.parseInt(document.getElementById('input').value);
-    let score=Number.parseInt(document.getElementById('score').innerText);
     if(val == 9){
         
         alert("You have entered correct answer");
         score++;
-        document.getElementById('score').innerHTML=score;
+        
     }
         
     else{
         alert("You have enterd wrong answer");
         score--;
-        document.getElementById('score').innerHTML=score;
+        
     }
+    document.getElementById('score').innerHTML=score;
         
     console.log(score);
-
-    //alert(`The form is Submitted Score is ${score}`);
     
 }
